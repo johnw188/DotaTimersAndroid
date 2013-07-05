1944e83dd389d684e0ebfe42eba71b9c30f7f558
John Welsh <john.welsh@workday.com>
2013-07-05 02:49:52 -0700

    work

---
+++ DotaTimers/src/main/java/com/johnwelsh/dotatimers/views/CountdownOverlay.java
@@ -0,0 +1,50 @@
+package com.johnwelsh.dotatimers.views;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.johnwelsh.dotatimers.R;
+
+/**
+ * Created by john.welsh on 7/5/13.
+ */
+public class CountdownOverlay extends View {
+    private Paint textPaint;
+
+    private int initialTime;
+    private int currentTime;
+
+    public void startTimer(int numberOfSeconds) {
+        initialTime = numberOfSeconds;
+        currentTime = numberOfSeconds;
+    }
+
+    public void decrementTimer() {
+        currentTime = currentTime - 1;
+        invalidate();
+    }
+
+    public CountdownOverlay(Context context) {
+        super(context);
+    }
+
+    public CountdownOverlay(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CountdownOverlay(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    private void init() {
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        int textColor = getResources().getColor(R.color.countdown_text);
+        textPaint.setColor(textColor);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+
+    }
+
+}
---
+++ DotaTimers/src/main/java/com/johnwelsh/dotatimers/views/TimelineView.java
@@ -0,0 +1,14 @@
+package com.johnwelsh.dotatimers.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by john.welsh on 7/5/13.
+ */
+public class TimelineView extends View {
+    public TimelineView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
